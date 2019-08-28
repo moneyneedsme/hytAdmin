9d5754d76a371deb986c1822f7ad99b424f90542
moneyneedsme <1311746495@qq.com>
2019-08-28 10:54:55 +0800

    修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -54,9 +54,6 @@ router.beforeEach( async (to, from, next) => {
       name: homeName // 跳转到homeName页
     })
   } else {
-    if(!from.name){ //页面刷新 
-      refreshRoute();
-    }
     // console.log(store._mutations.setRoutersList)
     if (store.state.user.hasGetInfo) {   //判断是否获取到数据
       // turnTo(to, store.state.user.access, next)   //store.state.user.access  == ['super_admin'] ['super_admin', 'admin']
@@ -65,6 +62,10 @@ router.beforeEach( async (to, from, next) => {
         refreshRoute();
       }
     } else {
+      // next();
+      // if(!from.name){ //页面刷新 
+      //   refreshRoute();
+      // }
       store.dispatch('getUserInfo').then(user => {
         // 拉取用户信息，通过用户权限和跳转的页面的name来判断是否有权限访问;access必须是一个数组，如：['super_admin'] ['super_admin', 'admin']
         // turnTo(to, user.access, next)
